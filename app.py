9227c217eae92c61f2edf4ec6827b22b64dfbee7
Anh Nguyen <nguyenhoanganha1y@gmail.com>
2023-03-06 09:28:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask.cli import with_appcontext
 import models
 from models import db
+from resource.user import UsersResource
 import random
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+api = Api(app)
+api.add_resource(UsersResource, "/users/")
 
 with app.app_context():
     """
