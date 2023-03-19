a0ecdc32c516e942ef6f2ebd471609eec0684dbd
Bonny Ham <tram.nguyen@student.oulu.fi>
2023-03-20 01:14:53 +0200

    Create models for playlist,tracks,playlisttrack and user. Write flask routes for playlist management and unit tests

--- models.py
+++ models.py
@@ -1,40 +1,119 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy as sa
+
+
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    __tablename__ = "User"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    user_name  = db.Column(db.String(64), nullable=False, unique=True)
-    password  = db.Column(db.String(64), nullable=False)
-    #TODO: Define schema here later
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def get_schema_not_dedicate(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+        }
+
+    @staticmethod
+    def get_schema(self):
+        return {
+            "type": "object",
+            "properties": {
+                "id": {"type": "integer"},
+                "username": {"type": "string", "required": True},
+                "password": {"type": "string", "required": True},
+            }
+        }
 
 
+# Playlist model
+# the Playlist model has a relationship with the User model using the user_id foreign key
 class Playlist(db.Model):
-    __tablename__ = "Playlist"
+    __tablename__ = 'playlists'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable=False)
-    date_created = db.Column(db.String(64), nullable=False, default="descending")
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)  # format: yyyy-mm-dd hh:mm:ss
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('playlists', lazy=True))
 
-    user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
-    songs = db.relationship("Song", secondary="In_Playlist")
-    
-    #TODO: Define schema here later
+    def get_schema_not_dedicate(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created_at": self.created_at,
+            "user": self.user.get_schema()
+        }
+
+    @staticmethod
+    def get_schema(self):
+        return {
+            "type": "object",
+            "properties": {
+                "id": {"type": "integer"},
+                "name": {"type": "string", "required": True},
+                "created_at": {"type": "string", "format": "date-time", "required": True},
+                "user_id": {"type": "integer", "required": True},
+                "user": {"type": "object", "required": True, "properties": User.get_schema()['properties']}
+            }
+        }
 
 
-class Song(db.Model):
-    __tablename__ = "Song"
+# PlaylistTrack model
+class PlaylistTrack(db.Model):
+    __tablename__ = 'playlist_tracks'
     id = db.Column(db.Integer, primary_key=True)
-    tittle = db.Column(db.String(64), nullable=False)
-    directory = db.Column(db.String(256), nullable=False)
-    genre = db.Column(db.String(19), nullable=False)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'), nullable=False)
+    playlist = db.relationship('Playlist', backref=db.backref('playlist_tracks', lazy=True))
+    track_id = db.Column(db.Integer, db.ForeignKey('track.id'), nullable=False)
+    track = db.relationship('Track', backref=db.backref('playlist_tracks', lazy=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('playlist_tracks', lazy=True))
+
+    def get_schema_not_dedicate(self):
+        return {
+            "id": self.id,
+            "playlist": self.playlist.get_schema(),
+            "track": self.track.get_schema(),
+            "user": self.user.get_schema()
+        }
+
+    @staticmethod
+    def get_schema(self):
+        return {
+            'id': self.id,
+            'playlist_id': self.playlist_id,
+            'track_id': self.track_id,
+            'user_id': self.user_id
+        }
+
+
+# Track model
+class Track(db.Model):
+    __tablename__ = 'tracks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    artist = db.Column(db.String(80), nullable=False)
     duration = db.Column(db.Integer, nullable=False)
-    #TODO: Define schema here later
 
+    def get_schema_not_dedicate(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "artist": self.artist,
+            "duration": self.duration
+        }
 
-class Playlist_track(db.Model):
-    __tablename__ = "Playlist_track"
-
-    playlist_id = db.Column(sa.ForeignKey("Playlist.id"), primary_key=True)
-    song_id = db.Column(sa.ForeignKey("Song.id"), primary_key=True)
-    track_number = db.Column(sa.Integer)
+    @staticmethod
+    def get_schema(self):
+        return {
+            "type": "object",
+            "properties": {
+                "id": {"type": "integer"},
+                "name": {"type": "string", "required": True},
+                "artist": {"type": "string", "required": True},
+                "duration": {"type": "integer", "required": True}
+            }
+        }
