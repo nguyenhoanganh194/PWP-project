4653ef9c9927dbac9e277a0e11dc62de3520384d
Tarek_ali <tarekredaali@gmail.com>
2023-05-01 23:45:37 +0200

    more client edits

--- client/app.py
+++ client/app.py
@@ -17,13 +17,13 @@ main_user_flag=0
 second_user_flag=0
 main_user_data=[]
 second_user_data=[]
+main_user_playlists=[]
 
 
 def threaded_task(matchinguser):
     global second_user_data
     global second_user_flag
-    user= generator.find_user(s,matchinguser, body["@controls"]["plm:users-all"]["href"],SERVER_URL)
-    playlistdata,playlist_hrefs = generator.display_playlists(s,user,SERVER_URL)
+    playlistdata,playlist_hrefs,tracks_href = generator.display_playlists(s,matchinguser,SERVER_URL)
     second_user_data=generator.playlist_tracks(s,playlist_hrefs,SERVER_URL)
     second_user_flag=1
 
@@ -45,18 +45,25 @@ def landing():
 
 @app.route('/playlists', methods = ['GET', 'POST'])  
 def playlists():
-    user=request.form['username']
-    matchinguser=request.form['matchinguser'] 
-    thread = Thread(target=threaded_task, args=(matchinguser,))
-    thread.daemon = True
-    thread.start()
-    user= generator.find_user(s,user, body["@controls"]["plm:users-all"]["href"],SERVER_URL)
-    data,playlist_hrefs = generator.display_playlists(s,user,SERVER_URL)
-    thread2 = Thread(target=threaded_task2, args=(playlist_hrefs,))
-    thread2.daemon = True
-    thread2.start()
-    session['users'] = [user,matchinguser]
-    return render_template('playlists.html',data=data)
+    global main_user_playlists
+    username=request.form['username']
+    matchingusername=request.form['matchinguser'] 
+    user= generator.find_user(s,username, body["@controls"]["plm:users-all"]["href"],SERVER_URL)
+    matchinguser= generator.find_user(s,matchingusername, body["@controls"]["plm:users-all"]["href"],SERVER_URL)
+    if user == None or matchinguser == None:
+        return render_template('error.html')
+    else:
+        thread = Thread(target=threaded_task, args=(matchinguser,))
+        thread.daemon = True
+        thread.start()
+        main_user_playlists,playlist_hrefs,tracks_href = generator.display_playlists(s,user,SERVER_URL)
+        thread2 = Thread(target=threaded_task2, args=(playlist_hrefs,))
+        thread2.daemon = True
+        thread2.start()
+        session['users'] = [username,matchingusername]
+        session['tracks_href']= tracks_href
+        session['playlist_hrefs']= [playlist_hrefs]
+        return render_template('playlists.html',data=main_user_playlists)
 
 @app.route('/tracks', methods = ['GET', 'POST'])  
 def tracks():
@@ -76,7 +83,34 @@ def tracks():
 def matching():
     results, percent = generator.match_tracks(main_user_data,second_user_data)
     
-    return render_template('matching.html',data=results, percent = percent)
+    return render_template('matching.html',data=results, percent = percent,users=session.get('users'))
+
+@app.route('/addtrack', methods = ['GET', 'POST'])  
+def addtrack(): 
+    return render_template('addtrack.html',data=second_user_data,playlist_data=main_user_playlists)
+
+@app.route('/tracksuccess', methods = ['GET', 'POST'])  
+def tracksuccess():
+    track=request.form.getlist('track-name')
+    playlisthref=request.form['playlist-href'] 
+    response = s.get(SERVER_URL + session.get('tracks_href'))
+    body = response.json()
+    ctrl=body["@controls"]["plm:add-track"]
+    trackdata=[]
+    for item in track:
+        for data in second_user_data:
+            if item == data[0]:
+                trackdata.append(data)
+    locations =[]
+    for item in trackdata:
+        response = generator.add_track(s,ctrl,item,SERVER_URL)
+        print(response)
+        locations.append(response)
+
+    generator.add_track_to_playlist(s,locations,playlisthref,SERVER_URL)
+
+    return render_template('final.html')
+
 
 if __name__ == '__main__':
     with requests.Session() as s:
--- client/helper/generator.py
+++ client/helper/generator.py
@@ -1,4 +1,5 @@
 import difflib
+import json
 
 def playlist_tracks(s, playlist_track_href,SERVER_URL):
     data=[]
@@ -27,6 +28,7 @@ def display_playlists(s,user_href,SERVER_URL):
     resp = s.get(SERVER_URL + user_href)
     body = resp.json()
     playlists_href=body["@controls"]["plm:playlists-of"]["href"]
+    tracks_href= body["@controls"]["plm:tracks-of"]["href"]
     data =[]
     playlist_hrefs=[]
     resp = s.get(SERVER_URL + playlists_href)
@@ -39,9 +41,10 @@ def display_playlists(s,user_href,SERVER_URL):
         playlist_hrefs.append(body["@controls"]["plm:tracks_of_playlist"]["href"])
         item_data.append(body["item"]["name"])
         item_data.append(body["item"]["created_at"])
+        item_data.append(body["@controls"]["self"]["href"])
         data.append(item_data)
     
-    return data,playlist_hrefs 
+    return data,playlist_hrefs,tracks_href 
 
 def find_user_href(name, collection):
     hits = []
@@ -74,3 +77,47 @@ def match_tracks(main_user_data,second_user_data):
                 break 
     percent = len(list)/len(main_user_data) *100         
     return list, round(percent)
+
+
+def add_track(s, ctrl,trackdata,SERVER_URL):
+    data = {}
+    data["name"]=trackdata[0]
+    data["artist"]=trackdata[1]
+    data["duration"]=trackdata[2]
+    resp = s.request(
+        ctrl["method"],
+        SERVER_URL + ctrl["href"],
+        data=json.dumps(data),
+        headers = {"Content-type": "application/json"}
+    )
+    return resp.headers["Location"]
+
+
+def add_track_to_playlist(s,locations,playlisthref,SERVER_URL):
+    list=[]
+    for item in locations:
+        resp = s.get(SERVER_URL + item)
+        body = resp.json()
+        list.append(body["item"]["id"])
+    
+    response = s.get(SERVER_URL + playlisthref)
+    body = response.json()
+    id = body["item"]["id"]
+    tracks_of_playlist =body["@controls"]["plm:tracks_of_playlist"]["href"]
+
+    response = s.get(SERVER_URL + tracks_of_playlist)
+    body = response.json()
+    ctrl= body["@controls"]["plm:add-playlisttrack"]
+
+    for track in list:
+        data = {}
+        data["track_id"]=track
+        data["playlist_id"]=id
+        data["track_number"]=track
+        resp = s.request(
+            ctrl["method"],
+            SERVER_URL + ctrl["href"],
+            data=json.dumps(data),
+            headers = {"Content-type": "application/json"}
+        )
+
